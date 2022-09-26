eb78a8fd86ced712bce4b1ad25a7026df327f787
hauckshane <61210107+hauckshane@users.noreply.github.com>
2022-09-26 11:30:12 -0400

    Add files via upload

---
+++ exam1.sql
@@ -0,0 +1,44 @@
+CREATE view shauc19_q10  as
+WITH
+    t as(
+        SELECT
+            pno
+        FROM
+            parts
+        WHERE
+            pname = 'Bolt'
+    ),
+    s as (SELECT sno, quantity
+          FROM shipments,
+               t
+          WHERE t.pno = shipments.pno)
+SELECT
+    s.sno, sname, quantity
+FROM
+    s, suppliers
+WHERE
+    s.sno = suppliers.sno;
+
+CREATE view shauc19_q11 as
+SELECT
+    sno, shipments.pno, max(weight) * quantity as weight
+FROM
+    parts, shipments
+group by shipments.pno, sno, quantity;
+
+CREATE view shauc19_q12 as
+WITH
+    t as (SELECT city,
+                 count(*) as count
+          FROM shipments,
+               parts
+          where shipments.pno = parts.pno
+          group by city),
+    s as (SELECT max(count) as count
+          FROM t)
+SELECT
+    city
+FROM
+    t, s
+WHERE
+    t.count = s.count;
