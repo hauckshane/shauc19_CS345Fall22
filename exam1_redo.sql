80766440b804acf3de95eff215eca6a283ac85cc
hauckshane <61210107+hauckshane@users.noreply.github.com>
2022-10-10 14:46:10 -0400

    Add files via upload

---
+++ exam1_redo.sql
@@ -0,0 +1,28 @@
+
+CREATE view shauc19_q11 as
+WITH t as (SELECT sno, -- get weights of all shipments
+                  shipments.pno,
+                  weight * quantity as weight
+           FROM parts,
+                shipments),
+    s as (SELECT max(weight) -- find the maximum weight value
+          FROM t)
+SELECT sno, pno, weight -- get the shipping number, parts number and weight of heaviest shipment
+FROM t, s
+WHERE s.max = t.weight;
+
+
+
+CREATE view shauc19_q12 as
+WITH
+    t as (SELECT city, -- get number of shipments for each supplier city
+                 count(*) as count
+          FROM shipments,
+               suppliers
+          where shipments.sno = suppliers.sno
+          group by city),
+    s as (SELECT max(count) as count -- find the maximum value of shipments for each supplier city
+          FROM t)
+SELECT city -- select the supplier city that has the maximum value of shipments
+FROM t, s
+WHERE t.count = s.count;
